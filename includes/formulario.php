a87c4ffe6d99978fa30ca1760b8dec60707b28ea
Paulo <corazza.paulovinicius@gmail.com>
2021-07-26 23:29:40 -0300

    fim login

--- includes/formulario.php
+++ includes/formulario.php
@@ -1,55 +1,55 @@
 <main>
-
   <section>
     <a href="index.php">
       <button class="btn btn-warning">Voltar</button>
     </a>
   </section>
-
   <h2 class="mt-3"><?=TITLE?></h2>
-
   <form method="post">
-
-    <div class="form-group">
-      <label>Título</label>
-      <input type="text" class="form-control" name="titulo" value="<?=$vaga->titulo?>">
-    </div>
-
-    <div class="form-group">
-      <label>Descrição</label>
-      <textarea class="form-control" name="descricao" rows="5"><?=$vaga->descricao?></textarea>
-    </div>
-
-    <div class="form-group">
-      <label>Salário</label>
-      <input type="text" class="form-control" name="salario" value="<?= $vaga->salario ?>" >
-    </div>
-
-    <div class="form-group">
-      <label>Status</label>
-
-      <div>
-          <div class="form-check form-check-inline">
-            <label class="form-control">
-              <input type="radio" name="ativo" value="s" checked> Ativo
-            </label>
-          </div>
-
-          <div class="form-check form-check-inline">
-            <label class="form-control">
-              <input type="radio" name="ativo" value="n" <?=$vaga->ativo == 'n' ? 'checked' : ''?>> Inativo
-            </label>
-          </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label>Título</label>
+          <input type="text" class="form-control" name="titulo" value="<?=$vaga->titulo?>">
+        </div>
       </div>
-
+      <div class="col-md-6">
+        <div class="form-group">
+          <label>Salário</label>
+          <input type="text" class="form-control" name="salario" value="<?= $vaga->salario ?>">
+        </div>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-group">
+          <label>Descrição</label>
+          <textarea class="form-control" name="descricao" rows="5"><?=$vaga->descricao?></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <div class="form-check form-check-inline">
+              <label class="form-control">
+                <input type="radio" name="ativo" value="s" checked> Ativo
+              </label>
+            </div>
 
-
-
+            <div class="form-check form-check-inline">
+              <label class="form-control">
+                <input type="radio" name="ativo" value="n" <?=$vaga->ativo == 'n' ? 'checked' : ''?>> Inativo
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="form-group">
       <button type="submit" class="btn btn-success">Enviar</button>
     </div>
-
   </form>
-
 </main>
--- index.php
+++ index.php
@@ -9,33 +9,40 @@ use App\Session\Login;
 //obriga o usuário a estar logado
 Login::requireLogout();
 
+$alertaLogin = '';
+$alertaCadastro = '';
+
 if(isset($_POST['acao'])){
    
     switch($_POST['acao']){
         case 'logar':
 
+            //busca o usuario pelo email
             $usuario = Usuario::getUsuarioPorEmail($_POST['email']);
 
-            if(!$usuario instanceof Usuario){
+            if(!$usuario instanceof Usuario || !password_verify($_POST['senha'], $usuario->senha)){
+                $alertaLogin = 'E-mail ou senha não conferem!';
                 break;
             }
 
-            echo '<pre>'; 
-            print_r($usuario); 
-            echo '</pre>'; exit;
+            Login::login($usuario);
            
         case 'cadastrar':
             if(isset($_POST['nome'],$_POST['email'],$_POST['senha'])){
                 
+                $usuario = Usuario::getUsuarioPorEmail($_POST['email']);
+                if($usuario instanceof Usuario){
+                    $alertaCadastro = 'O email cadastrado já está em uso!';
+                    break;
+                }
+
                 $usuario = new Usuario();
                 $usuario->nome = $_POST['nome'];
                 $usuario->email = $_POST['email'];
                 $usuario->senha =  password_hash($_POST['senha'], PASSWORD_DEFAULT);
                 $usuario->cadastrar();
 
-                echo '<pre>'; 
-                print_r($usuario); 
-                echo '</pre>'; exit;
+                Login::login($usuario); 
 
             }
         
